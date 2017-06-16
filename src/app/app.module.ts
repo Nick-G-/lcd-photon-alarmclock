a701c6377954446e1cfccc0503c024e906c6b324
Nicolás Gargano <nicolas.gargano@ing.austral.edu.ar>
2017-06-16 08:49:20 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import 'hammerjs';
+import { NguiDatetimePickerModule } from '@ngui/datetime-picker';
 
 import { AppComponent } from './app.component';
 
+// Material Design
+import { MaterialModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { PhotonControlsComponent } from './photon-controls/photon-controls.component';
+import {ParticleService} from './partice-service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotonControlsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    NguiDatetimePickerModule
   ],
-  providers: [],
+  providers: [ParticleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
